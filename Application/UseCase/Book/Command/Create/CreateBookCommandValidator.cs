3c7ffecc8ba84131e71a3577e65f342d4da76477
Matheus Bertho <matheus.tavares@me.com.br>
2023-03-17 12:23:59 -0300

    add validator testes

--- Application/UseCase/Book/Command/Create/CreateBookCommandValidator.cs
+++ Application/UseCase/Book/Command/Create/CreateBookCommandValidator.cs
@@ -7,5 +7,15 @@ public class CreateBookCommandValidator : AbstractValidator<CreateBookCommand>
     public CreateBookCommandValidator()
     {
         RuleFor(x => x.Title).MinimumLength(5);
+        RuleFor(x => x.Url).MinimumLength(5);
+        RuleFor(x => x.Price).GreaterThan(0d);
+
+        RuleFor(x => x.AuthorId).NotEmpty()
+            .Must(id => Guid.TryParse(id.ToString(), out _))
+            .WithMessage("The Value for 'AuthorId' must be a Valid Guid.");
+
+        RuleFor(x => x.CategoryId).NotEmpty()
+            .Must(id => Guid.TryParse(id.ToString(), out _))
+            .WithMessage("The Value for 'CategoryId' must be a Valid Guid.");
     }
 }
---
+++ UnitTest/Book/CreateBookValidationTest.cs
@@ -0,0 +1,62 @@
+﻿using Application.UseCase.Book.Command.Create;
+using FluentValidation.TestHelper;
+
+namespace UnitTest.Book;
+
+public class CreateBookValidationTest
+{
+    private CreateBookCommandValidator _validator = null!;
+
+
+    [SetUp]
+    public void SetUp()
+    {
+        _validator = new CreateBookCommandValidator();
+    }
+
+
+    [Test]
+    public void Validate_TitleLessThanFiveCharacters_ValidationError()
+    {
+        var command = new CreateBookCommand
+        {
+            Title = "1234",
+            Url = "http://test.com",
+            Price = 10.0,
+            AuthorId = Guid.NewGuid().ToString(),
+            CategoryId = Guid.NewGuid().ToString()
+        };
+        var result = _validator.TestValidate(command);
+        result.ShouldHaveValidationErrorFor(x => x.Title);
+    }
+
+    [Test]
+    public void Validate_UrlLessThanFiveCharacters_ValidationError()
+    {
+        var command = new CreateBookCommand
+        {
+            Title = "Test Book",
+            Url = "1234",
+            Price = 10.0,
+            AuthorId = Guid.NewGuid().ToString(),
+            CategoryId = Guid.NewGuid().ToString()
+        };
+        var result = _validator.TestValidate(command);
+        result.ShouldHaveValidationErrorFor(x => x.Url);
+    }
+
+    [Test]
+    public void Validate_PriceEqualToZero_ValidationError()
+    {
+        var command = new CreateBookCommand
+        {
+            Title = "Test Book",
+            Url = "http://test.com",
+            Price = 0.0,
+            AuthorId = Guid.NewGuid().ToString(),
+            CategoryId = Guid.NewGuid().ToString()
+        };
+        var result = _validator.TestValidate(command);
+        result.ShouldHaveValidationErrorFor(x => x.Price);
+    }
+}
